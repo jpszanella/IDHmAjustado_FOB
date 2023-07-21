57bdae9a0814ba2c3d87f5dcc74848333894393f
João Pedro S Zanella <123907394+jpszanella@users.noreply.github.com>
2023-07-21 13:37:15 -0300

    merging_data.py

--- merging_data.py
+++ merging_data.py
@@ -1,3 +1,6 @@
 # Combinar as DataFrames com base nas colunas em comum
 merged_data = pd.merge(dataexpo, idhm, left_on=['UF do Produto','Ano'], right_on=['UF','Ano'])
 display(merged_data)
+# Describe statistical resume
+resumo_estatistico = merged_data.describe()
+display(resumo_estatistico)
