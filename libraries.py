dfaf8244c352764fdbf7c813ec4d25318cc7e39c
João Pedro S Zanella <123907394+jpszanella@users.noreply.github.com>
2023-07-21 13:26:13 -0300

    libraries.py

---
+++ libraries.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
